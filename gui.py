7f90859c0eb59d43e6ad8aacabed86dde10249c7
Jen <jennifer.stemkowski@gmail.com>
2024-04-13 12:25:37 -0400

    Create input fields + labels

--- gui.py
+++ gui.py
@@ -12,4 +12,9 @@ canvas.pack()
 app_label = Label(root, text="QR Code Generator", fg = 'blue', font = ('Arial', 30))
 canvas.create_window(200, 50, window = app_label)
 
+name_label = Label(root, text="Link Name: ")
+link_label = Label(root, text="Link: ")
+canvas.create_window(200, 100, window = name_label)
+canvas.create_window(200, 160, window = link_label)
+
 root.mainloop()
