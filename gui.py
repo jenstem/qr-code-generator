b4295fe66a29d4aa7e52ccb280ce6f085a478380
Jen <jennifer.stemkowski@gmail.com>
2024-04-13 12:28:10 -0400

    Add user entry fields

--- gui.py
+++ gui.py
@@ -9,12 +9,21 @@ root = Tk()
 canvas = Canvas(root, width=400, height=600)
 canvas.pack()
 
+
 app_label = Label(root, text="QR Code Generator", fg = 'blue', font = ('Arial', 30))
 canvas.create_window(200, 50, window = app_label)
+
 
 name_label = Label(root, text="Link Name: ")
 link_label = Label(root, text="Link: ")
 canvas.create_window(200, 100, window = name_label)
 canvas.create_window(200, 160, window = link_label)
+
+
+name_entry = Entry(root)
+link_entry = Entry(root)
+canvas.create_window(200, 130, window = name_entry)
+canvas.create_window(200, 180, window = link_entry)
+
 
 root.mainloop()
