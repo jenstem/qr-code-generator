189aeca5b153be346981a37f191d8cf9425dc22c
Jen <jennifer.stemkowski@gmail.com>
2024-04-13 12:22:43 -0400

    Add label to canvas

--- gui.py
+++ gui.py
@@ -9,6 +9,7 @@ root = Tk()
 canvas = Canvas(root, width=400, height=600)
 canvas.pack()
 
-app_label = Label(root, text="QR Code Generator", fg = 'blue', font=('Arial', 30))
+app_label = Label(root, text="QR Code Generator", fg = 'blue', font = ('Arial', 30))
+canvas.create_window(200, 50, window = app_label)
 
 root.mainloop()
